e75db15da99ed146f00487bc4f631a3fcf10d9d2
Oliveira Prado <EliasOPrado@users.noreply.github.com>
2019-04-09 17:04:33 +0100

    addition of other formats such as J L O S Z I

--- tetris.js
+++ tetris.js
@@ -16,7 +16,7 @@ function collide(arena, player) {
         for (let x = 0; x < m[y].length; ++x) {
             if (m[y][x] !== 0 &&
                 (arena[y + o.y] &&
-                arena[y + o.y][x + o.x]) !== 0) {
+                    arena[y + o.y][x + o.x]) !== 0) {
                 return true;
             }
         }
@@ -32,12 +32,61 @@ function createMatrix(w, h) {
     return matrix;
 }
 
+function createPiece(type) {
+    if (type === 'T') {
+        return [
+           [0, 0, 0],
+           [1, 1, 1],
+           [0, 1, 0],
+        ];
+    } else if (type === 'O') {
+        return [
+           [1, 1],
+           [1, 1],
+        ];
+    } else if (type === 'L') {
+        return [
+           [0, 1, 0],
+           [0, 1, 0],
+           [0, 1, 1],
+        ];
+    } else if (type === 'J') {
+        return [
+           [0, 1, 0],
+           [0, 1, 0],
+           [1, 1, 0],
+        ];
+    } else if (type === 'I') {
+        return [
+           [0, 1, 0, 0],
+           [0, 1, 0, 0],
+           [0, 1, 0, 0],
+           [0, 1, 0, 0],
+        ];
+    } else if (type === 'S') {
+        return [
+           [0, 1, 1],
+           [1, 1, 0],
+           [0, 0, 0],
+        ];
+    } else if (type === 'Z') {
+        return [
+           [1, 1, 0],
+           [0, 1, 1],
+           [0, 0, 0],
+        ];
+    }
+}
+
 function draw() {
 
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawMatrix(arena, {x:0, y:0});
+    drawMatrix(arena, {
+        x: 0,
+        y: 0
+    });
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -67,10 +116,10 @@ function merge(arena, player) {
 
 function playerDrop() {
     player.pos.y++;
-    if (collide(arena, player)){
+    if (collide(arena, player)) {
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
     }
     dropCounter = 0;
 }
@@ -78,39 +127,53 @@ function playerDrop() {
 function playerMove(dir) {
     player.pos.x += dir;
     if (collide(arena, player)) {
-        player.pos.x -= dir; 
+        player.pos.x -= dir;
     }
 }
 
-function playerRotate(dir){
+function playerReset(){
+    const pieces = 'TJLOSZI';
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) -
+                   (player.matrix[0].length / 2 | 0);
+    if (collide(arena, player)){
+        arena.forEach(row => row.fill(0));
+    }
+}
+
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
     let offset = 1;
     rotate(player.matrix, dir);
-    while (collide(arena, matrix)){
+    while (collide(arena, player)) {
         player.pos.x += offset;
         offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].lenght){
+        if (offset > player.matrix[0].lenght) {
             rotate(player.matrix, -dir);
+            player.pos.x = pos;
             return;
         }
     }
 }
 
-function rotate(matrix, dir){
-   for (let y = 0; y < matrix.length; ++y) {
-       for (let x = 0; x < y; ++x){
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
            [
                matrix[x][y],
                matrix[y][x],
-           ]=[
+           ] = [
                matrix[y][x],
                matrix[x][y],
            ];
-       }
-   } 
-    
-    if (dir > 0){
+        }
+    }
+
+    if (dir > 0) {
         matrix.forEach(row => row.reverse())
-    }else{
+    } else {
         matrix.reverse();
     }
 }
@@ -139,7 +202,7 @@ const player = {
         x: 5,
         y: 5
     },
-    matrix: matrix,
+    matrix: createPiece('T'),
 }
 
 document.addEventListener('keydown', event => {
